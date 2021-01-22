f80d2d456d70062c26c7b4b680f0cc4f654b729e
yuHuang01 <huang.y0112@gmail.com>
2021-01-22 12:32:18 +0100

    basic styling is done

---
+++ form.js
@@ -0,0 +1,10 @@
+const myInputs = {
+	firstName: document.getElementById('firstName'),
+	lastName: document.getElementById('lastName'),
+	email: document.getElementById('email'),
+	zipCode: document.getElementById('zipCode'),
+	country: document.getElementById('country'),
+	password: document.getElementById('password'),
+	passwordValid: document.getElementById('passwordValid')
+};
+
